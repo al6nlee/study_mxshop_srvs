4a5e4b4ef891e2ecd04484ef1ca8780c59d4594c
alan <al6nlee@gmail.com>
2024-12-20 14:27:06 +0800

    完善库存逻辑

--- inventory_srv/handler/inventory.go
+++ inventory_srv/handler/inventory.go
@@ -1,9 +1,81 @@
 package handler
 
 import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"study_mxshop_srvs/inventory_srv/global"
+	"study_mxshop_srvs/inventory_srv/model"
 	"study_mxshop_srvs/inventory_srv/proto"
 )
 
 type InventoryServer struct {
 	proto.UnimplementedInventoryServer
+}
+
+func (*InventoryServer) SetInv(ctx context.Context, req *proto.GoodsInvInfo) (*emptypb.Empty, error) {
+	// 设置库存， 如果我要更新库存
+	var inv model.Inventory
+	global.DB.Where(&model.Inventory{Goods: req.GoodsId}).First(&inv)
+	inv.Goods = req.GoodsId
+	inv.Stocks = req.Num
+
+	global.DB.Save(&inv)
+	return &emptypb.Empty{}, nil
+}
+
+func (*InventoryServer) InvDetail(ctx context.Context, req *proto.GoodsInvInfo) (*proto.GoodsInvInfo, error) {
+	var inv model.Inventory
+	if result := global.DB.Where(&model.Inventory{Goods: req.GoodsId}).First(&inv); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "没有库存信息")
+	}
+	return &proto.GoodsInvInfo{
+		GoodsId: inv.Goods,
+		Num:     inv.Stocks,
+	}, nil
+}
+
+func (*InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
+	// 扣减库存， 本地事务 [1:10,  2:5, 3: 20]
+	// 数据库基本的一个应用场景：数据库事务
+	// 并发情况之下 可能会出现 超卖 现象 ---> 分布式锁
+	tx := global.DB.Begin()
+	for _, goodInfo := range req.GoodsInfo {
+		var inv model.Inventory
+		if result := global.DB.Where(&model.Inventory{Goods: goodInfo.GoodsId}).First(&inv); result.RowsAffected == 0 {
+			tx.Rollback() // 回滚之前的操作
+			return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
+		}
+		if inv.Stocks < goodInfo.Num {
+			tx.Rollback() // 回滚之前的操作
+			return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
+		}
+		// 扣减
+		inv.Stocks -= goodInfo.Num
+		tx.Save(&inv)
+	}
+	tx.Commit() // 需要自己手动提交操作
+	return &emptypb.Empty{}, nil
+}
+
+func (*InventoryServer) Reback(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
+	// 库存归还：
+	// 1：订单超时归还
+	// 2. 订单创建失败，归还之前扣减的库存
+	// 3. 手动归还
+	tx := global.DB.Begin()
+	for _, goodInfo := range req.GoodsInfo {
+		var inv model.Inventory
+		if result := global.DB.Where(&model.Inventory{Goods: goodInfo.GoodsId}).First(&inv); result.RowsAffected == 0 {
+			tx.Rollback() // 回滚之前的操作
+			return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
+		}
+
+		// 并发上去后，会出现数据不一致的问题 - 锁，分布式锁
+		inv.Stocks += goodInfo.Num
+		tx.Save(&inv)
+	}
+	tx.Commit()
+	return &emptypb.Empty{}, nil
 }
--- inventory_srv/model/inventory.go
+++ inventory_srv/model/inventory.go
@@ -20,6 +20,14 @@ type BaseModel struct {
 // 	Address string
 // }
 
+// type InventoryHistory struct {
+// 	user   int32
+// 	goods  int32
+// 	nums   int32
+// 	order  int32
+// 	status int32 // 1. 表示库存是预扣减，幂等性， 2. 表示已经支付
+// }
+
 type Inventory struct {
 	BaseModel
 	Goods   int32 `gorm:"type:int;index"` // 跨服务了，不能使用外键；另外这个字段使用比较多，所以设置一个索引
